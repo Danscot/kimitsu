2ab438ec5665fa1db18ed9e2dcdcae0fc5df5393
Danscot <scotfielddaniel@gmail.com>
2026-02-25 23:59:45 +0100

    fixed profile

--- auth_sys/models.py
+++ auth_sys/models.py
@@ -1,52 +1,106 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
-
 from django.utils import timezone
-
 from datetime import timedelta
 
 
 class CustomUser(AbstractUser):
 
-	SUBSCRIPTION_CHOICES = [
+    email = models.EmailField(unique=True)
 
-		('free', '24 hours'),
-		('500', ' 14 Jours'),
-		('1000', '30 Jours'),
-	]
+    USERNAME_FIELD = 'email'
 
-	bot_number = models.JSONField(default=list) 
+    REQUIRED_FIELDS = []
 
-	email = models.EmailField(unique=True)
+    email_ver = models.BooleanField(default=False)
 
-	USERNAME_FIELD = 'email'
+    ver_code = models.PositiveIntegerField(null=True, blank=True)
 
-	REQUIRED_FIELDS = ['username']
+    bot_number = models.JSONField(default=list, blank=True)
 
-	bot_exp = models.JSONField(default=list) 
+    bot_exp = models.JSONField(default=list, blank=True)
 
-	ver_code = models.PositiveIntegerField(default=0, null=True)
+    paired_num = models.PositiveIntegerField(default=0)
 
-	email_ver = models.BooleanField(default=False)
+    paired_lim = models.PositiveIntegerField(default=1)
 
-	sub_format = models.CharField(max_length=20, choices=SUBSCRIPTION_CHOICES, verbose_name="Catégorie", default=SUBSCRIPTION_CHOICES[0])
+    SUBSCRIPTION_CHOICES = [
 
-	paired_num = models.PositiveIntegerField(default=0)
+        ('Aucun', 'Aucun abonnement'),
+        ('free', '24 heures'),
+        ('500', '14 Jours'),
+        ('1000', '30 Jours'),
+    ]
 
-	paired_lim = models.PositiveIntegerField(default=1)
+    sub_format = models.CharField(
 
-	def __str__(self):
+        max_length=20,
+        choices=SUBSCRIPTION_CHOICES,
+        default='Aucun'
+    )
 
-		return self.username
+    subscription_expiry = models.DateTimeField(null=True, blank=True)
 
-	def can_pair(self):
+    payment_method = models.CharField(max_length=50, null=True, blank=True)
 
-		if self.paired_lim == self.self.paired_num:
+    def __str__(self):
+        return self.email
 
-			return False
+    def can_pair(self):
 
-		else:
+        return self.paired_num < self.paired_lim
 
-			return True
+    def activate_subscription(self, plan_code):
 
+        self.sub_format = plan_code
+
+        if plan_code == 'free':
+
+            self.subscription_expiry = timezone.now() + timedelta(hours=24)
+
+            self.paired_lim = 1
+
+        elif plan_code == '500':
+
+            self.subscription_expiry = timezone.now() + timedelta(days=14)
+
+            self.paired_lim = 2
+
+        elif plan_code == '1000':
+
+            self.subscription_expiry = timezone.now() + timedelta(days=30)
+
+            self.paired_lim = 5
+
+        else:
+
+            self.subscription_expiry = None
+
+            self.paired_lim = 1
+
+        self.save()
+
+    # 🔹 Check if subscription is active
+    def is_subscription_active(self):
+
+        if self.subscription_expiry:
+
+            return timezone.now() < self.subscription_expiry
+            
+        return False
+
+    # 🔹 Auto downgrade if expired
+    def check_subscription_status(self):
+        if self.subscription_expiry and timezone.now() >= self.subscription_expiry:
+            self.sub_format = 'Aucun'
+            self.subscription_expiry = None
+            self.paired_lim = 1
+            self.save()
+
+    # 🔹 Get price (for templates)
+    def get_price(self):
+        if self.sub_format == '500':
+            return 500
+        elif self.sub_format == '1000':
+            return 1000
+        return 0
